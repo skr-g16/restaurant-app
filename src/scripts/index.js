db4420648cf77dc29a3be46b7d54625753f39cf4
skr-g16 <fahmirty@gmail.com>
2024-10-16 20:28:14 +0700

    fix navbar and hero content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,12 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
+const burger = document.getElementById("burger");
+const navLinks = document.querySelector(".nav-links");
+
+burger.addEventListener("change", () => {
+  if (burger.checked) {
+    navLinks.classList.add("open");
+  } else {
+    navLinks.classList.remove("open");
+  }
+});
