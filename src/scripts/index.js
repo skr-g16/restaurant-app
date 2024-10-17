c7af79566ceb544431dc5d0b3926d2b0b3176c04
skr-g16 <fahmirty@gmail.com>
2024-10-17 12:46:18 +0700

    add build file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,9 @@ import "../styles/main.css";
 import "../styles/responsive.css";
 import renderData from "./render-data.js";
 import dataRestaurant from "../public/data/DATA.json";
-
 import toggleBurger from "./toggle-burger.js";
-toggleBurger();
-renderData(dataRestaurant);
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  toggleBurger();
+  renderData(dataRestaurant);
+});
--- src/scripts/render-data.js
+++ src/scripts/render-data.js
@@ -4,17 +4,17 @@ const renderData = (data) => {
   data.restaurants.forEach((restaurant) => {
     listOfRestaurant.innerHTML += `
       <div class="card" id="card-${restaurant.id}">
-       <div class="card2">
-        <div class="card-image">
-          <img class="image" src="${restaurant.pictureId}" alt="${restaurant.name}">
+        <div class="card2">
+          <div class="card-image">
+            <img tabindex="0" class="image" src="${restaurant.pictureId}" alt="${restaurant.name}">
+          </div>
+          <div class="card-content">
+            <h3 class="name" tabindex="0">${restaurant.name}</h3>
+            <p class="city" tabindex="0">${restaurant.city}</p>
+            <p class="description" tabindex="0">${restaurant.description}</p>
+            <p class="rating" tabindex="0"><strong>Rating:</strong> ⭐${restaurant.rating}</p>
+          </div>
         </div>
-        <div class="card-content">
-          <h3 class="name">${restaurant.name}</h3>
-          <p class="city">${restaurant.city}</p>
-          <p class="description">${restaurant.description}</p>
-          <p class="rating"><strong>Rating:</strong> ⭐${restaurant.rating}</p>
-        </div>
-       </div>
       </div>
       `;
   });
