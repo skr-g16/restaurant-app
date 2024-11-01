f54a692a64141c9fa5be1b8e1b3caec657b547d7
skr-g16 <fahmirty@gmail.com>
2024-11-01 17:58:53 +0700

    add webmanifest with icon

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: {
@@ -11,6 +12,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     clean: true,
   },
   module: {
@@ -56,6 +58,23 @@ module.exports = {
               maxAgeSeconds: 30 * 24 * 60 * 60,
             },
           },
+        },
+      ],
+    }),
+    new WebpackPwaManifest({
+      name: 'Steak Angkringan',
+      short_name: ' Restaurant Catalogue',
+      description: 'Pedagang Kaki Lima Rasa Bintang Lima',
+      background_color: '#fb412c',
+      theme_color: '#091523',
+      start_url: '/index.html',
+      display: 'standalone',
+      icons: [
+        {
+          src: path.resolve('./src/public/icons/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('icons'),
+          purpose: 'any',
         },
       ],
     }),
