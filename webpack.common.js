b89847ceda38162bdd9b0380333812c1f8f6bd1d
skr-g16 <fahmirty@gmail.com>
2024-11-09 18:59:19 +0700

    move clean webpack to production

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -25,9 +26,12 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
+
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/templates/index.html'),
+      // favicon: path.resolve(__dirname, 'src/public/images/logo/logo_trans.png'),
     }),
     new CopyWebpackPlugin({
       patterns: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -21,5 +20,4 @@ module.exports = merge(common, {
       },
     ],
   },
-  plugins: [new CleanWebpackPlugin()],
 });
